8db13a72b3e8ab3ff81ab7a9319656d98e48c046
Usman Jamil <usmanjamilkamboh7@gmail.com>
2026-08-12 10:16:01 +0500

    Mode : Easy

---
+++ valid_parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+        for ch in s :
+            if ch == "(" or ch == "{" or ch == "[" :
+                stack.append(ch)
+            else :
+                if not stack :
+                    return False
+                elif stack[-1] == pairs[ch]:
+                    stack.pop()
+                else :
+                    return False 
+
+        if not stack :
+            return True 
+        else:
+            return False 
+                

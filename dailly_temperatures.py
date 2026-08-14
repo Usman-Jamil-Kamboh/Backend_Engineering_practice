a42e2df6d030ea64e25e522b28f808bc8214db3d
Usman Jamil <usmanjamilkamboh7@gmail.com>
2026-08-14 16:44:45 +0500

    Mode : Medium

---
+++ dailly_temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)):
+
+            while stack and temperatures[i] > stack[-1][0] :
+                answer[stack[-1][1]] = i - stack[-1][1]
+                stack.pop()
+
+            stack.append((temperatures[i] , i )) 
+        return answer
